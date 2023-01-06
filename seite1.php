096cec5a2dc1d0c42337a3ff6853d42ee1c7d0cc
Wolfgang Sesin <39748663+firewall-1@users.noreply.github.com>
2023-01-06 14:07:46 +0100

    Delete seite1.php

--- seite1.php
+++
@@ -1,5 +0,0 @@
-<form action="seite2.php" method="post">
-Vorname: <input type="text" name="vorname" /><br />
-Namename: <input type="text" name="nachname" /><br />
-<input type="Submit" value="Absenden" />
-</form
